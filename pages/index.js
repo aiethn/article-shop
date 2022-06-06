4677feb6153d33d8d4ff9b175c53b9722b714302
Faqih Ethana Prabandaru <faqihethana@mail.ugm.ac.id>
2022-06-07 00:51:09 +0700

    add feature searching article in home

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ const optionsDay = [
 export default function Home() {
   const dispatch = useDispatch();
   const [data, setData] = useState("");
+  const [searchValue, setSearchValue] = useState("");
   const [selectedData, setSelectedData] = useState("");
   const [filterCat, setFilterCat] = useState(optionsCat[0]);
   const [filterDay, setFilterDay] = useState(optionsDay[0]);
@@ -38,7 +39,14 @@ export default function Home() {
     const newStart = 4 * (page - 1);
     const newEnd = 4 * page;
     setSelectedData(data.slice(newStart, newEnd));
-  }, [data, page]);
+    if (data && searchValue) {
+      const filterArticleByUser = data.filter((article) => {
+        const searchContent = article.title + article.byline + article.abstract;
+        return searchContent.toLowerCase().includes(searchValue.toLowerCase());
+      });
+      setSelectedData(filterArticleByUser);
+    }
+  }, [data, page, searchValue]);
 
   const handleOnChange = (sel, filter) => {
     if (filter === "category") {
@@ -92,53 +100,86 @@ export default function Home() {
 
   return (
     <div>
-      <div className="flex justify-end">
-        <Select
-          instanceId="1"
-          isSearchable={false}
-          value={filterCat}
-          defaultValue={optionsCat[0]}
-          onChange={(e) => handleOnChange(e, "category")}
-          options={optionsCat}
-          className="mr-4"
-        />
-        <Select
-          instanceId="2"
-          isSearchable={false}
-          value={filterDay}
-          defaultValue={optionsDay[0]}
-          onChange={(e) => handleOnChange(e, "day")}
-          options={optionsDay}
-        />
+      <div className="flex md:flex-row flex-col md:space-y-0 space-y-4 justify-between">
+        <div className="relative">
+          <label>
+            <input
+              placeholder="Search articles"
+              onChange={(e) => setSearchValue(e.target.value)}
+              className="block md:w-96 w-full px-4 py-3 text-gray-900 border-0 bg-gray-200 bg-opacity-50 rounded-md  focus:ring-primary-500 focus:border-primary-500 "
+              type="text"
+            />
+            <svg
+              className="absolute w-6 h-6 text-gray-400 right-3 top-3 "
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="black"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </label>
+        </div>
+        <div className="flex">
+          <Select
+            instanceId="1"
+            isSearchable={false}
+            value={filterCat}
+            defaultValue={optionsCat[0]}
+            onChange={(e) => handleOnChange(e, "category")}
+            options={optionsCat}
+            className="mr-4"
+          />
+          <Select
+            instanceId="2"
+            isSearchable={false}
+            value={filterDay}
+            defaultValue={optionsDay[0]}
+            onChange={(e) => handleOnChange(e, "day")}
+            options={optionsDay}
+          />
+        </div>
       </div>
       {!data || !selectedData || isLoading ? (
         <SkeletonHome />
       ) : (
-        <div className="flex flex-wrap">
-          {selectedData.map((article) => (
-            <Card
-              key={article.id}
-              articleId={article.id}
-              title={article.title}
-              tags={article.section}
-              date={article.published_date}
-              author={article.byline}
-              desc={article.abstract}
-              link={article.url}
-              imageLink={
-                article.media?.[0]?.["media-metadata"]?.[2].url
-                  ? article.media?.[0]["media-metadata"]?.[2].url
-                  : article.media?.[0]?.["media-metadata"]?.[1].url
-                  ? article.media?.[0]["media-metadata"]?.[1].url
-                  : article.media?.[0]?.["media-metadata"]?.[0].url
-                  ? article.media?.[0]["media-metadata"]?.[0].url
-                  : "/banner.jpg"
-              }
-            />
-          ))}
-        </div>
+        <>
+          {!selectedData.length && searchValue && (
+            <div className="w-full sm:h-96 h-60 flex text-center justify-center items-center">
+              <p className="font-bold text-xl">No Article Found.</p>
+            </div>
+          )}
+          <div className="flex flex-wrap">
+            {selectedData.map((article) => (
+              <Card
+                key={article.id}
+                articleId={article.id}
+                title={article.title}
+                tags={article.section}
+                date={article.published_date}
+                author={article.byline}
+                desc={article.abstract}
+                link={article.url}
+                imageLink={
+                  article.media?.[0]?.["media-metadata"]?.[2].url
+                    ? article.media?.[0]["media-metadata"]?.[2].url
+                    : article.media?.[0]?.["media-metadata"]?.[1].url
+                    ? article.media?.[0]["media-metadata"]?.[1].url
+                    : article.media?.[0]?.["media-metadata"]?.[0].url
+                    ? article.media?.[0]["media-metadata"]?.[0].url
+                    : "/banner.jpg"
+                }
+              />
+            ))}
+          </div>
+        </>
       )}
-      <Pagination page={page} handlePage={handlePage} />
+      {!searchValue && <Pagination page={page} handlePage={handlePage} />}
     </div>
   );
 }
